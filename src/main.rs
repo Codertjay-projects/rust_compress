dfe11f5ed1167844b539841053ec764fcf253be3
Codertjay <codertjay@gmail.com>
2022-10-10 11:15:27 +0100

    in

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+extern crate flate2;
+
+use std::env::args;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::copy;
+use std::time::Instant;
+
+use flate2::Compression;
+use flate2::write::GzEncoder;
+
+
+fn main() {
+    if args().len() != 3 {
+        eprintln!("Usage: `source` `target`");
+        return;
+    }
+    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    let output = File::create(args().nth(2).unwrap()).unwrap();
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    let start = Instant::now();
+    copy(&mut input, &mut encoder).unwrap();
+    let output = encoder.finish().unwrap();
+    println!("Source len: {:?}", input.get_ref().metadata().unwrap().len());
+    println!("Target len: {:?}", output.metadata().unwrap().len());
+    println!("Elapsed: {:?}", start.elapsed());
+}
